831d0ab1ee99afd5f4dd27a10bd86e0cb2271cb3
Mario Rubio <mrubio.sopra@grupogimeno.onmicrosoft.com>
2024-10-02 17:15:47 +0200

    feat: Initial commit

---
+++ src/main/java/es/marugi/learn/spring_webflux/SpringWebfluxApplication.java
@@ -0,0 +1,13 @@
+package es.marugi.learn.spring_webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebfluxApplication.class, args);
+	}
+
+}
